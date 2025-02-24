8d12a0b7bf591c40ae020c0b602ca0cf402828dd
jbrisco188 <26933081-jbrisco188@users.noreply.replit.com>
2025-02-24 17:23:38 +0000

    Added README

---
+++ main.py
@@ -0,0 +1,16 @@
+#---imports 
+
+#---functions
+def intro_to_game(): #introduces the game and asks for the user's name/class/stats
+  print("Welcome to Text Based RPG!")
+  print("What is your name?")
+  name = input()
+
+def display_map(): #shows the map so the user can see where they are
+  print("-----------------")
+
+
+def ask_for_action(): #character movement etc
+  print("What would you like to do?")
+
+#---main
