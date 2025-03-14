0e10ea656da05858ab694ed292ae6ad98c584440
JoshuaBrisco <146298084+JoshuaBrisco@users.noreply.github.com>
2025-03-14 08:23:15 +0000

    Worked on more classes, then updated the readme with more info

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 Text Based RPG Game
+
+To do:
+  Design game "engine"
+    Explore mode
+      Player movement arround an array type map
+    Combat mode
+      Combat will occur occastionally, likely a chance based encounter based on the 
+      tile that they are on
+      Will loop each char in the fight's turns until health <= 0
+    All modes
+      Certain functions will need to be available no matter what mode we are in, they
+      will be called with a text "letter", to be determined ("i" for inventory)
+  Design how combat will function
+    Math and interaction between attack/defense
+    May add additional stats(speed, magic) for additional class distinction (stretch        goal)
+      
+      
--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 #---imports 
+import math
 
 #---objects
 
-class charater():
+class character():
   def __init__(self, name, health, attack, defense):
     self.name = name 
     self.health = health
     self.attack = attack
     self.defense = defense
+    
+  def showCurrentStats(self):
+    print(f"Health: {self.health}, Attack: {self.attack}, Defense: {self.defense}" )
 
-class Player(charater):
+
+class Player(character):
+  def __init__(self, name, health, attack, defense, inv):
+    super().__init__(name, health, attack, defense)
+    self.inv = inv #will contain a list, initial inv may change based on what class
+
+
+class Fighter(Player):
+  def __init__(self, name, health, attack, defense, inv):
+    super().__init__(name, health, attack, defense, inv)
+
+  def powerAttack(self):
+    print("Going for a big attack") #proof of concept, will replace once more of the game engine is in
+
+class Enemy(character):
   def __init__(self, name, health, attack, defense):
     super().__init__(name, health, attack, defense)
 
@@ -28,3 +46,17 @@ def ask_for_action(): #character movement etc
   print("What would you like to do?")
 
 #---main
+
+print("Welcome to Text Based RPG!")
+print("Choose your class!")
+
+char = input("Enter "f" for figher: ")
+#will need error control here for when users enter a wrong input
+
+
+
+#Testing
+  
+#player = Fighter("Josh", 50, 10, 5, [])
+#player.showCurrentStats()
+#player.powerAttack()
